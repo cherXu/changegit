9a9ce2b74fcf451a20172b68f378825206b60820
cher_Xu@wistronits.com <cher_Xu@wistronits.com>
2016-01-05 09:09:53 +0800

    add 2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+222222222
 var widoApp = angular.module('widoApp', [
     'ionic', 'ngCordova', 'pascalprecht.translate', 'ngFileUpload'
 ]);
